4114e1396d15029cfdac9807af98267d2eeb0cb0
Aratani-star <masahiroaratani387@gmail.com>
2025-03-10 08:31:03 +0900

    feat:/ move config info to env file

--- apps/frontend/lib/firebaseConfig.ts
+++ apps/frontend/lib/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBW1aKzFjfoiLA600yjQvCYhJX9e2m3-Y8",
-  authDomain: "signin-ea76e.firebaseapp.com",
-  projectId: "signin-ea76e",
-  storageBucket: "signin-ea76e.firebasestorage.app",
-  messagingSenderId: "764608279876",
-  appId: "1:764608279876:web:e1bff1243479124e1b1311",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
